b76d4198d1b1677f6d07ca8e6af09eacc40ddc47
HiiimNick <nick.ligmann1503@gmail.com>
2026-05-10 22:20:43 +0200

    fix icon styling

--- src/tui/util/mod.rs
+++ src/tui/util/mod.rs
@@ -16,7 +16,7 @@ pub fn map_block_color(app: &App, expected: CurrentFocus) -> Style {
 pub fn map_status_to_span(status: String, conclusion: Option<String>) -> Span<'static> {
     match status.as_str() {
         "queued" | "pending" | "requested" => Span::styled(" ", Style::default().fg(Color::White)),
-        "in_progress" => Span::styled("", Style::default().fg(Color::White)),
+        "in_progress" => Span::styled(" ", Style::default().fg(Color::White)),
         "action_required" | "waiting" => Span::styled(" ", Style::default().fg(Color::Yellow)),
         "completed" => {
             if let Some(conclusion) = conclusion {
